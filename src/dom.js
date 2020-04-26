459e979dde47cfba30731bf6b85472ab758b00fb
Tony G <tlguzman94@gmail.com>
2020-04-26 15:22:47 -0400

    added functionality to load project items when clicked.

--- src/dom.js
+++ src/dom.js
@@ -1,9 +1,22 @@
-// 
+import { getLocalProjects } from './index'
 
-function addProject(project) {
+function addProject(project, index) {
   const projectEle = document.createElement('li');
-  projectEle.textContent = project.getTitle();
+  const title = document.createElement('h3');
+  title.textContent = project.getTitle();
+  projectEle.value = index;
+  projectEle.appendChild(title);
   document.querySelector('#project-list').appendChild(projectEle);
+  addProjectEvent(projectEle);
+}
+
+function addProjectEvent(element) {
+  let projectList = getLocalProjects();
+  element.addEventListener('click', () => {
+    let index = +element.value;
+    selectProject(projectList[index]);
+    console.log('selected project: ' + projectList[index].getTitle());
+  });
 }
 
 function addTodo(todo) {
@@ -14,11 +27,45 @@ function addTodo(todo) {
   title.textContent = todo.getTitle();
   todoEle.appendChild(checkbox);
   todoEle.appendChild(title);
-
   document.querySelector('#todo-list').appendChild(todoEle);
 }
+
+function addTodoToProject(todo) {
+  let projectList = getLocalProjects();
+  let title = document.querySelector('#todo-title').textContent;
+  for (let project of projectList) {
+    if(project.getTitle().toUpperCase() === title) {
+      project.addTodo(todo);
+      console.log(`add ${todo.getTitle()} to ${project.getTitle()}`);
+    }
+  }
+}
+
+function loadTodoList(project) {
+  for (let todo of project.getTodoList()) {
+    addTodo(todo);
+  }
+}
+
+function selectProject(project) {
+  clearList();
+  setTodoTitle(project.getTitle());
+  loadTodoList(project);
+}
+
+function setTodoTitle(title) {
+  document.querySelector('#todo-title').textContent = title.toUpperCase();
+}
+
+function clearList() {
+  document.querySelector('#todo-list').textContent = '';
+}
+
 
 export { 
   addProject, 
   addTodo,
+  loadTodoList,
+  selectProject,
+  addTodoToProject,
 };
--- src/index.js
+++ src/index.js
@@ -1,34 +1,45 @@
 import {Todo} from './todo';
 import {Project} from './project'
-import {addProject, addTodo} from './dom'
+import {addProject, addTodo, selectProject, addTodoToProject} from './dom'
+
 
 let localProjects = [];
 
-let todo = Todo('title', 'des', 'due', 'p');
-let todo2 = Todo('title2', 'des2', 'due2', 'p2');
-let todoList = [];
-todoList.push(todo);
-todoList.push(todo2);
+let project = Project('project 1', []);
+let todo = Todo('task 1', 'des', 'due', 'p');
+let todo2 = Todo('task 2', 'des2', 'due2', 'p2');
+project.addTodo(todo);
+project.addTodo(todo2);
+localProjects.push(project);
+
+addProject(localProjects[0], 0);
+selectProject(localProjects[0]);
 
 
 document.querySelector('#submit-project').addEventListener('click', (e) => {
   let title = document.querySelector('#project-input').value;
   if (title) {
     let project = Project(title, []);
-    addProject(project);
+    addProject(project, localProjects.length);
     localProjects.push(project);
   }
   document.querySelector('#add-project').reset();
   e.preventDefault();
 });
 
-
 document.querySelector('#submit-todo').addEventListener('click', (e) => {
   let title = document.querySelector('#title-input').value;
   if (title) {
     let todo = Todo(title);
     addTodo(todo);
+    addTodoToProject(todo);
   }
   document.querySelector('#add-todo').reset();
   e.preventDefault();
 });
+
+function getLocalProjects() {
+  return localProjects;
+}
+
+export { getLocalProjects };
